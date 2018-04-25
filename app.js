9615e90b5f2fd25be77e28e631c2b09cac9b94a8
NelsonElios <alnelson780@gmail.com>
2018-04-25 15:14:08 +0200

    add new movie form

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
-
+const PORT =3000;
 const server = express();
+let movies = [];
 
 server.use('/css', express.static('css'));//middleware pour servir des reponses statiques comme les css
 server.set('views','./views');
 server.set('view engine', 'ejs');// indiquer que l'on utilise ejs comme moteur de template
 
+//var urlencodedParser = bodyParser.urlencoded({extend: false});
+server.use(bodyParser.urlencoded({extended: false}));// le middleware sera utilisé de manière globale quand on en a besoin
 server.get('/',
     (req, res)=> {
     //res.send('bonjour la compagnie');
@@ -23,7 +27,49 @@ server.get('/movies/add' ,
     (req, res) => {
     res.send('formulaire');
     }
-    )
+    );
+
+//let movieTitre;
+
+server.post('/movies',
+    (req, res) => {
+    const newMovie = {
+        title: req.body.movie,
+        year : req.body.year,
+    };
+    movies = [...movies,newMovie];
+
+            console.log(movies);
+        }
+
+
+
+)
+
+server.get('/movies',
+    (req ,res) => {
+     movies = [
+        {
+            title: 'terminator',
+            year: 1996
+        },
+        {
+            title: 'terminator',
+            year: 1991
+        },
+         {
+             title: 'avenger infinity war',
+             year: 2018
+         }
+
+        ];
+    res.render('movies',{films: movies,movieTitre: null});
+
+    }
+    );
+
+//var urlencoded = bodyParser.urlencoded({extend:false});
+
 
 server.get('/movie/:id/:title', // les : c'est exclusivement pour definir des paramètres
     (req,res) => {
@@ -34,9 +80,9 @@ server.get('/movie/:id/:title', // les : c'est exclusivement pour definir des pa
 
 
 
-server.listen('3000',
+server.listen(PORT,
     () => {
-    console.log('serveur demarrer');
+    console.log('serveur demarré');
     }
 
 )
